1e0fdb6a9de10e7103faa454f2ebf877ece65b4b
Owen Luke <hi.owen.luke@gmail.com>
2019-07-10 11:00:35 +0800

    .

--- test/queryGenerator.test.js
+++ test/queryGenerator.test.js
@@ -1,25 +1,25 @@
 
-const seqWhere = require('../src');
+const seqQuery = require('../src');
 const expect = require('chai').expect;
 
 describe('QueryGenerator', () => {
-	seqWhere.init('mssql');
+	seqQuery.init('mssql');
 
 	it('.getWhereConditions()', () => {
 		const where = {"id": {$eq: 2}};
-		const whereStr = seqWhere.getWhereConditions(where);
+		const whereStr = seqQuery.getWhereConditions(where);
 		expect(whereStr === '[id] = 2').to.be.true;
 	});
 
 	it('.getOrderClause()', () => {
 		const order = ['type', 'name desc'];
-		const orderStr = seqWhere.getOrderClause(order);
+		const orderStr = seqQuery.getOrderClause(order);
 		expect(orderStr === 'order by type, name desc').to.be.true;
 	});
 
 	it('.getLimitClause()', () => {
 		const limitOffset = {limit: 10, offset: 5};
-		const limitStr = seqWhere.getLimitClause(limitOffset);
+		const limitStr = seqQuery.getLimitClause(limitOffset);
 		expect(limitStr === 'offset 5 rows fetch next 10 rows only').to.be.true;
 	});
 });
