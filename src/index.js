20db9b2ff59434a113965b8b6b6b9075ab6e33f0
Owen Luke <hi.owen.luke@gmail.com>
2019-07-11 14:45:43 +0800

    attach Sequelize.Op to sequery

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@
 const config = require('./config');
 const exec = require('./exec');
 const queryGenerator = require('./queryGenerator');
+const Sequelize = require('sequelize');
 
 const me = Object.assign(Object.create(queryGenerator), {
 	init(cfg) {
 		config.init(cfg);
 		exec.init();
 		queryGenerator.init();
+
+		this.Op = Sequelize.Op;
 	},
 
 	async do(...args) {
