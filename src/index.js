5c423c9ef7bb5f4fb13e47e56ee77ed912980563
Owen Luke <hi.owen.luke@gmail.com>
2019-07-12 13:14:25 +0800

    Create a namespace in global: global.__sequelize_raw_query = {queryGenerator}

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ const Sequelize = require('sequelize');
 const me = Object.assign(Object.create(queryGenerator), {
 	init(cfg) {
 
+		// Create a namespace in global to save queryGenerator so that
+		// the entire user project with multi-sequery uses the same queryGenerator
+		global.__sequelize_raw_query = {queryGenerator: undefined};
+
 		config.init(cfg);
 		exec.init();
 		queryGenerator.init();
